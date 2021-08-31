e412a7dc17b0944722c81457ce78f001ceb37e19
Ryan De Souza <ryandman117@gmail.com>
2021-08-31 08:56:26 -0400

    added beef

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloClass
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Mr. Lesieur!");
+            Console.WriteLine("Beef");
         }
     }
 }
